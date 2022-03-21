10ac16f609051df82f762060a201de209c6bd530
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-03-20 22:16:13 -0300

    Finish stuff
    Co-authored-by: aliceborges42 <aliceborges42@users.noreply.github.com>
    Co-authored-by: Caio Massucato <CaioMassucato@users.noreply.github.com>
    Co-authored-by: Lucas <LucasVinic@users.noreply.github.com>
    Co-authored-by: José Costa <viniciusgarreto@users.noreply.github.com>

--- src/Classes.cpp
+++ src/Classes.cpp
@@ -1,4 +1,6 @@
 #include "../headers/Classes.h"
+#include "../headers/ConstantPool.h"
+#include "../headers/definitions.h"
 
 #include <iostream>
 
@@ -17,4 +19,15 @@ Classes::Classes(FILE* fp) {
     << "[CLASSES]\tthis->inner_name: " << (unsigned) this->inner_name << endl
     << "[CLASSES]\tthis->inner_class_access_flags: " << (unsigned) this->inner_class_access_flags << endl
 	<< endl;
+}
+
+void Classes::print(ConstantPool& cp, unsigned int indent) {
+	indentBy(indent);
+  cout << "inner_class_info: "<< (unsigned) this->inner_class_info << endl;
+	indentBy(indent);
+  cout << "outer_class_info: "<< (unsigned)  this->outer_class_info << endl;
+  indentBy(indent);
+  cout << "inner_name: "<< (unsigned)  this->inner_name << endl;
+  indentBy(indent);
+  cout << "inner_class_access_flags: "<< (unsigned)  this->inner_class_access_flags << endl;
 }
