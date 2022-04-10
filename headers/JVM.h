dd46e7c9cf52bd4f537fbae794fe328576acd390
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-04-10 20:15:18 -0300

    Create basic JVM class structure

--- headers/JVM.h
+++ headers/JVM.h
@@ -1,38 +1,26 @@
 #ifndef jvm_h
 #define jvm_h
 
-#include "definitions.h"
 #include <vector>
-#include "Frame.h"
+#include "../headers/definitions.h"
+#include "../headers/StackFrame.h"
+#include "../headers/ClassFile.h"
 
 using namespace std;
 
-typedef struct frameStack {
-  list<frame> frames;
-} FrameStack;
-
-/*
-* TODO: implementar as classes ClassesJVM, FramesPile e ObjectList
-*       Criar método construtor de JVM
-*
-*/
-
-
-
 class JVM {
   public:
-
+    JVM();
+    ~JVM();
 
   private:
-    // Each Java Virtual Machine thread has a private Java Virtual Machine stack, created
-    // at the same time as the thread.
-
-    //ClassesJVM* classes;
-    vector<FrameStack> frame_stack;
-    //ObjectList* object;
     u4 pc;
     u2 exception;
     char *exception_name;
+    StackFrame* frame_stack;
+    vector<ClassFile>* classes;
+    // TODO: incluir lista de objetos
+    
 };
 
 #endif
---
+++ headers/StackFrame.h
@@ -0,0 +1,12 @@
+#ifndef stack_frame_h
+#define stack_frame_h
+
+class StackFrame {
+  public:
+    StackFrame();
+    ~StackFrame();
+
+  private:
+};
+
+#endif
