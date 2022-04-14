9d7cc16f0a2139fe082fabcc7430e8547f0de607
CaioMassucato <caiobernardon@gmail.com>
2022-04-14 18:44:19 -0300

    Refactor Instructions

--- headers/Instructions.h
+++ headers/Instructions.h
@@ -8,14 +8,17 @@ using namespace std;
 
 class Instruction {
   public:
+    Instruction(string name, int opcode, int argnum, int opnum);
+    ~Instruction();
     void setInstructions();
+    static Instruction& getInstruction(int opcode);
 
     // print methods
     ostream& print(ostream& output = cout) const;
     friend ostream& operator<<(ostream& out, const Instruction& cf);
 
   private:
-    Instruction* instruc;
+    static Instruction** instruc;
     string name;
     int opcode;
     int argnum;
