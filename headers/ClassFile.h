604a6ff7ee1eb123290f70bc02416693f4e994b7
aliceborges_42 <aliceborges@cjr.org.br>
2022-04-15 01:15:25 -0300

    Documentação do CLassFIle e Constant Pool

--- headers/ClassFile.h
+++ headers/ClassFile.h
@@ -59,17 +59,17 @@ class ClassFile {
     friend ostream& operator<<(ostream& out, const ClassFile& cf);
 
   private:
-    u4 magic;
-    u2 minor_version;
-    u2 major_version;
-    ConstantPool* constant_pool;
-    u2 access_flags;
-    u2 this_class;
-    u2 super_class;
-    Interfaces* interfaces;
-    Fields* fields;
-    Methods* methods;
-    Attributes* attributes;
+    u4 magic;                     // Identifica o formato do class file, possui o valor 0xCAFEBABE.
+    u2 minor_version;             // O menor numero de versão do class file.
+    u2 major_version;             // O maior numero de versao do class file, junto do minor version, determina a versao do formato do class file.
+    ConstantPool* constant_pool;  // Tabela de estruturas representando constantes de string, classes, nome de interfaces, fields e outras contantes que sao referenciadas dentro das estruturas e subestruturas do class file.
+    u2 access_flags;              // O valor do access flags e uma mascara de flags usadas para denotar permissões de acesso e propriedades de classe ou interface
+    u2 this_class;                // Um valor valido de index dentro da table de constant pool
+    u2 super_class;               // Um valor valido de index dentro da table de constant pool ou zero
+    Interfaces* interfaces;       // Um array de valores validos de index dentro da table de constant pool
+    Fields* fields;               // Devem ser do tipo field info que da uma descrição completa de um field dessa classe ou interface
+    Methods* methods;             // Devem ser do tipo methods info que da uma descrição completa de um metodo dessa classe ou interface
+    Attributes* attributes;       // Devem ser do tipo attribute info
 };
 
 #endif
--- headers/Frame.h
+++ headers/Frame.h
@@ -9,15 +9,25 @@
 
 using namespace std;
 
+/**
+* @file Frame.h
+* @brief Define a estrutura de um único Frame 
+*/
+
+/// Operando e seu tipo
 struct Op {
+  /// Operando
   i4 operand;
+  /// Tipo do operando
   u1 operand_type;
 };
 
 struct OpList {
+  /// Lista de operandos
   list<Op> OpList;
 };
 
+/// Variaveis locais
 typedef struct local {
   u4 *variavel;
   u1 tipo_variavel;
@@ -25,11 +35,12 @@ typedef struct local {
 
 class Frame {
   public:
+    /// construtor
     Frame(string currentClass, u2 max_locals);
     ~Frame();
 
   private:
-    u4 end_retorno;
+    u4 end_retorno;                         
     u2 vetor_length;
     string classeCorrente;
     ConstantPool& constant_pool;
