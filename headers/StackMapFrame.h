6d6c2939234dfe1e9ddc8f8132bf3a17ae3a64b4
aliceborges_42 <aliceborges@cjr.org.br>
2022-04-15 21:36:29 -0300

    atualiza documentacao stack map frame

--- headers/StackMapFrame.h
+++ headers/StackMapFrame.h
@@ -23,16 +23,10 @@ Alunos:
 
 /**
 * @file StackMapFrame.h
-* @brief Classe Stack Map Frame definida pelo atributo Stack Map Table,
-union stack_map_frame {
-  same_frame;
-  same_locals_1_stack_item_frame;
-  same_locals_1_stack_item_frame_extended;
-  chop_frame;
-  same_frame_extended;
-  append_frame;
-  full_frame;
-}
+* @brief Classe Stack Map Frame definida pelo atributo Stack Map Table
+Um Stack Map Frame especifica (explícita ou implicitamente) o deslocamento de bytecode em
+que se aplica, e os tipos de verificação de variáveis locais e pilha de operandos
+entradas para esse deslocamento.
 */
 class StackMapFrame {
   public:
