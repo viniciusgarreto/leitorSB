4e59976a29c6ef701825a4c138a4652fb49c67ed
aliceborges_42 <aliceborges@cjr.org.br>
2022-04-15 21:47:40 -0300

    adiciona readme como main page

--- documents/html/search/pages_0.js
+++ documents/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['comando_20para_20execução_3a_0',['Comando para execução:',['../md_README.html',1,'']]]
+  ['comando_20para_20execução_3a_0',['Comando para execução:',['../index.html',1,'']]]
 ];
